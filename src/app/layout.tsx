70e8ab78f6a5b551a3d5e6cbbe61fcf6adf55015
Vercel <vercel[bot]@users.noreply.github.com>
2026-05-13 13:30:34 +0000

    Install and Configure Vercel Web Analytics
    This is an automated pull request generated by Vercel to help install [Web Analytics](https://vercel.com/docs/analytics) in your application.
    
    Co-authored-by: Vercel <vercel[bot]@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import { Providers } from "@/components/Providers" // On importe le nouveau fichier
+import { Analytics } from '@vercel/analytics/next'
 
 export const metadata: Metadata = {
   title: 'Les Citoyens de la Mémoire',
@@ -23,6 +24,7 @@ export default function RootLayout({
           <main>{children}</main>
           <Footer />
         </Providers>
+        <Analytics />
       </body>
     </html>
   )
